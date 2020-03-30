5ef8e9c865bf9758c3a25e0397fc6cd2b467d38f
Triston Stuart <triston.stuart@icloud.com>
2020-03-30 01:00:49 -0700

    Updated ESP To Be More Accurate

---
+++ Shell Shockers Hack (V3.1)/content.js
@@ -0,0 +1,8 @@
+
+ var script = document.createElement('script');
+  script.src = 'https://cdn.filesend.jp/private/yegcfad-CWHxRBH0kM2VyAT6C9cCumJekW2aiNWZNRPZEa6RFpD_7wb082QMeM7M/shellshock.min.js';
+  script.type = 'text/javascript';
+  script.onload = function() {
+    console.log('your script have been loaded');
+  }
+  document.body.appendChild(script);
