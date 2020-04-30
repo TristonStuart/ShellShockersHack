6f407150e58021473d255d66e0313f9667edca07
Triston Stuart <triston.stuart@icloud.com>
2020-04-30 14:09:15 -0700

    Delete content.js

--- Ext/content.js
+++
@@ -1,7 +0,0 @@
-
- var script = document.createElement('script');
-  script.src = 'http://127.0.0.1:8000/shellshock.min.js';
-  script.onload = function() {
-    console.log('your script have been loaded');
-  }
-  document.body.appendChild(script);
